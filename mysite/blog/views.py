c00d9f468a2be423cb7b49fb9331f688e4146559
Marco Belo <marco.barone.belo@gmail.com>
2024-10-09 20:03:42 -0300

    feat: add Paginator

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 
 from .models import Post
@@ -5,9 +6,14 @@ from .models import Post
 
 def post_list(request):
     posts = Post.published.all()
-    return render(request, "blog/post/list.html", {"posts": posts})
+    paginator = Paginator(posts, 3)
+    page_number = request.GET.get("page", 1)
+    posts_paginated = paginator.get_page(page_number)
+    return render(request, "blog/post/list.html", {"posts": posts_paginated})
 
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post, status=Post.Status.PUBLISHED, publish__year=year, publish__month=month, publish__day=day, slug=post
+    )
     return render(request, "blog/post/detail.html", {"post": post})
