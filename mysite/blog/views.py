8da8b09d93cffd00f878d844f7f5bc6a54a4e602
Marco Belo <marco.barone.belo@gmail.com>
2024-11-03 12:29:10 -0300

    feat: chapter 3 - add comment section to post detail

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,10 @@
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
+from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 
-from .forms import EmailPostForm
+from .forms import CommentForm, EmailPostForm
 from .models import Post
 
 
@@ -24,7 +25,9 @@ def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post, status=Post.Status.PUBLISHED, publish__year=year, publish__month=month, publish__day=day, slug=post
     )
-    return render(request, "blog/post/detail.html", {"post": post})
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(request, "blog/post/detail.html", {"post": post, "comments": comments, "form": form})
 
 
 class PostListView(ListView):
@@ -49,3 +52,15 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, "blog/post/share.html", {"post": post, "form": form, "sent": sent})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, "blog/post/comment.html", {"post": post, "form": form, "comment": comment})
